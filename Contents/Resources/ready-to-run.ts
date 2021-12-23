b31f2fc961c659452c7758a1369e02666a83249a
evanrs <me@evanrs.com>
2021-12-22 23:02:32 -0500

    feat: add parallel and sequential method to main

--- Contents/Resources/ready-to-run.ts
+++ Contents/Resources/ready-to-run.ts
@@ -1,14 +1,31 @@
 #! ./node_modules/.bin/ts-node
 
-import { $, argv } from "zx";
+import { argv } from "zx";
 import { allSettledFor } from "./tools";
 
 import * as protocol from "./protocol";
 
 async function main() {
   const [uri] = argv._;
+
+  await parallel(uri);
+}
+
+export async function parallel(uri: string) {
+  await allSettledFor(
+    [protocol.http, protocol.cmd].map((proto) => proto?.mount?.(uri))
+  );
+}
+
+// TODO: parameterize protocols
+export async function sequence(uri: string) {
+  const response = [];
   for (const route of [protocol.http, protocol.cmd]) {
-    route?.mount?.(uri);
+    try {
+      response.push(await route?.mount?.(uri));
+    } catch (response) {
+      console.log(`Unrouted response:`, response);
+    }
   }
 }
 
